033fd55e43538d7c09d9e963a2cc6594e39deda7
lenaskirdova <91190537+lenaskirdova@users.noreply.github.com>
2021-09-26 15:44:16 +0300

    Add files via upload

---
+++ 05_name/5_name.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+	
+char s[1000];
+setlocale(LC_ALL, "Russian");
+cin>>s;
+cout<<"Привет,";
+cout<<" "<<s<<"!"<<'\n';
+
+system("pause");
+return 0;
+}
+
+
+
+
