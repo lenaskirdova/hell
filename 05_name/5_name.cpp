63e46c74d3accbbc14849737c505e64e0cd7acc4
lenaskirdova <91190537+lenaskirdova@users.noreply.github.com>
2021-10-03 12:34:30 +0300

    Update 5_name.cpp

--- 05_name/5_name.cpp
+++ 05_name/5_name.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include <cstdlib>
+#include <Windows.h>
 using namespace std;
 
 int main()
 {
-	
+SetConsoleCP(1251);
+SetConsoleOutputCP(1251);
 char s[1000];
 setlocale(LC_ALL, "Russian");
 cin>>s;
-cout<<"Ïðèâåò,";
+cout<<"ÐŸÑ€Ð¸Ð²ÐµÑ‚,";
 cout<<" "<<s<<"!"<<'\n';
 
 system("pause");
