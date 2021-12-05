ce770bed072bc618b7b023c694ed28a96d138c62
lenaskirdova <91190537+lenaskirdova@users.noreply.github.com>
2021-12-06 02:22:35 +0300

    Add files via upload

---
+++ 19_FIFO/main2.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include "FIFO.h"
+
+int main()
+{
+    FIFO f = FIFO(5);
+    bool run = true;
+    while (run) {
+        std::cout << "1. show" << std::endl;
+        std::cout << "2. push" << std::endl;
+        std::cout << "3. pop" << std::endl;
+        std::cout << "4. exit" << std::endl;
+        std::cout << "> ";
+        char c;
+        std::cin >> c;
+        switch (c) {
+        case '1':
+            std::cout << f << std::endl;
+            break;
+        case '2':
+            int arg;
+            std::cout << "enter value: ";
+            std::cin >> arg;
+            f.push(arg);
+            break;
+        case '3':
+            std::cout << f.pop() << std::endl;
+            break;
+        case '4':
+            run = false;
+            break;
+        default:
+            std::cout << "incorrect option" << std::endl;
+            break;
+        }
+    }
+}
