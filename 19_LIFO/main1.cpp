e2b4c55d1645b2646f16de49c18c119d9dbb98cf
lenaskirdova <91190537+lenaskirdova@users.noreply.github.com>
2021-12-06 02:21:27 +0300

    Add files via upload

---
+++ 19_LIFO/main1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include "LIFO.h"
+
+int main()
+{
+    LIFO l = LIFO(5);
+    bool run = true;
+    while (run) {
+        std::cout << "1. show" << std::endl;
+        std::cout << "2. push" << std::endl;
+        std::cout << "3. pop" << std::endl;
+        std::cout << "4. exit" << std::endl;
+        std::cout << "> ";
+        char c;
+        std::cin >> c;
+        switch (c) {
+        case '1':
+            std::cout << l << std::endl;
+            break;
+        case '2':
+            int arg;
+            std::cout << "enter value: ";
+            std::cin >> arg;
+            l.push(arg);
+            break;
+        case '3':
+            std::cout << l.pop() << std::endl;
+            break;
+        case '4':
+            run = false;
+            break;
+        default:
+            std::cout << "incorrect option" << std::endl;
+            break;
+        }
+    }
+}
